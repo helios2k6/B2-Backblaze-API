46502e1335e435f192d57d47b1fd92c5a27d8fd8
Andrew Johnson <abjohnson@fb.com>
2018-06-22 22:29:50 -0700

    Adding the large file cancel action

---
+++ B2BackblazeBridge/Actions/CancelLargeFileUploadAction.cs
@@ -0,0 +1,82 @@
+/* 
+ * Copyright (c) 2015 Andrew Johnson
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in 
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using B2BackblazeBridge.Core;
+
+namespace B2BackblazeBridge.Actions
+{
+    /// <summary>
+    /// Represents a request to B2 to cancel the uploading of a large file
+    /// </summary>
+    public sealed class CancelLargeFileUploadAction : BaseAction<BackblazeB2CancelLargeFileUploadResult>
+    {
+        #region private fields
+        private readonly string APIURL = "/b2api/v1/b2_cancel_large_file";
+
+        private readonly BackblazeB2AuthorizationSession _authorizationSession;
+        private readonly string _fileId;
+        #endregion
+
+        #region public ctor
+        /// <summary>
+        /// Construct a new cancellation request for a large file upload
+        /// </summary>
+        /// <param name="authorizationSession">The authorization session to use</param>
+        /// <param name="fileId">The file to cancel the upload of</param>
+        public CancelLargeFileUploadAction(
+            BackblazeB2AuthorizationSession authorizationSession,
+            string fileId
+        ) : base(CancellationToken.None)
+        {
+            _authorizationSession = authorizationSession;
+            _fileId = fileId;
+        }
+        #endregion
+
+        #region public methods
+        public async override Task<BackblazeB2ActionResult<BackblazeB2CancelLargeFileUploadResult>> ExecuteAsync()
+        {
+            String getUploadUrlJsonStr = "{\"fileId\":\"" + _fileId + "\"}";
+            byte[] payload = Encoding.UTF8.GetBytes(getUploadUrlJsonStr);
+
+            HttpWebRequest webRequest = GetHttpWebRequest(_authorizationSession.APIURL + APIURL);
+            webRequest.Method = "POST";
+            webRequest.Headers.Add("Authorization", _authorizationSession.AuthorizationToken);
+            webRequest.ContentLength = payload.Length;
+
+            BackblazeB2ActionResult<BackblazeB2CancelLargeFileUploadResult> cancellationRequest =
+                await SendWebRequestAndDeserializeAsync<BackblazeB2CancelLargeFileUploadResult>(webRequest, payload);
+            if (cancellationRequest.HasResult)
+            {
+                string escapedFileName = cancellationRequest.Result.FileName;
+                cancellationRequest.Result.FileName = Uri.UnescapeDataString(escapedFileName);
+            }
+
+            return cancellationRequest;
+        }
+        #endregion
+    }
+}
---
+++ B2BackblazeBridge/Core/BackblazeB2CancelLargeFileUploadResult.cs
@@ -0,0 +1,95 @@
+/* 
+ * Copyright (c) 2015 Andrew Johnson
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in 
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using Newtonsoft.Json;
+using System;
+
+namespace B2BackblazeBridge.Core
+{
+    /// <summary>
+    /// The result returned from a cancellation request
+    /// </summary>
+    [Serializable]
+    [JsonObject(MemberSerialization.OptIn)]
+    public sealed class BackblazeB2CancelLargeFileUploadResult : IEquatable<BackblazeB2CancelLargeFileUploadResult>
+    {
+        #region public properties
+        /// <summary>
+        /// The account the bucket is in
+        /// </summary>
+        public string AccountID { get; set; }
+
+        /// <summary>
+        /// The bucket the file resides in
+        /// </summary>
+        public string BucketID { get; set; }
+
+        /// <summary>
+        /// The file ID that has been cancelled
+        /// </summary>
+        public string FileID { get; set; }
+
+        /// <summary>
+        /// The file name that has been cancelled
+        /// </summary>
+        public string FileName { get; set; }
+        #endregion
+
+        #region public methods
+        public bool Equals(BackblazeB2CancelLargeFileUploadResult other)
+        {
+            if (EqualsPreamble(other) == false)
+            {
+                return false;
+            }
+
+            return string.Equals(AccountID, other.AccountID, StringComparison.Ordinal) &&
+                string.Equals(BucketID, other.BucketID, StringComparison.Ordinal) &&
+                string.Equals(FileID, other.FileID, StringComparison.Ordinal) &&
+                string.Equals(FileName, other.FileName, StringComparison.Ordinal);
+        }
+
+        public override int GetHashCode()
+        {
+            return AccountID?.GetHashCode() ?? 0 ^
+                BucketID?.GetHashCode() ?? 0 ^
+                FileID?.GetHashCode() ?? 0 ^
+                FileName?.GetHashCode() ?? 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as BackblazeB2CancelLargeFileUploadResult);
+        }
+        #endregion
+
+        #region private methods
+        private bool EqualsPreamble(object other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (GetType() != other.GetType()) return false;
+
+            return true;
+        }
+        #endregion
+    }
+}
