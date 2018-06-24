a161af92d3bb1a91bc57122b8a6c429eb10f49db
Andrew Johnson <helios2k6@gmail.com>
2018-06-24 15:15:40 -0700

    Removing unsed usings

--- B2BackupUtility/CommonActions.cs
+++ B2BackupUtility/CommonActions.cs
@@ -25,8 +25,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace B2BackupUtility
