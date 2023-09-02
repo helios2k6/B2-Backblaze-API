7a6da57e9522bab6d4b7f11f80ac0e9fda27920e
Andrew Johnson <helios2k6@gmail.com>
2023-09-02 09:59:39 -0500

    Updating copyright

--- B2BackupUtility/Proxies/AuthorizationSessionProxy.cs
+++ B2BackupUtility/Proxies/AuthorizationSessionProxy.cs
@@ -1,5 +1,5 @@
 ﻿/* 
- * Copyright (c) 2015 Andrew Johnson
+ * Copyright (c) 2023 Andrew Johnson
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of 
  * this software and associated documentation files (the "Software"), to deal in 
